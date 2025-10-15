11a7a44ad1609afc2f732f4041c4bba8f74ba97d
Jordan Merli <jordanmerli90@gmail.com>
2025-10-15 14:15:24 +0200

    Timer PC

---
+++ Timer PC/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblSec = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cmbOperation = New System.Windows.Forms.ComboBox()
+        Me.bttnEnableDisable = New System.Windows.Forms.Button()
+        Me.bttnClose = New System.Windows.Forms.Button()
+        Me.lblRemain = New System.Windows.Forms.Label()
+        Me.timerOperate = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtMin = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblSec
+        '
+        Me.lblSec.Location = New System.Drawing.Point(9, 38)
+        Me.lblSec.Name = "lblSec"
+        Me.lblSec.Size = New System.Drawing.Size(195, 19)
+        Me.lblSec.TabIndex = 2
+        Me.lblSec.Text = "in secondi: 300"
+        Me.lblSec.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.cmbOperation)
+        Me.GroupBox2.Controls.Add(Me.bttnEnableDisable)
+        Me.GroupBox2.Controls.Add(Me.bttnClose)
+        Me.GroupBox2.Location = New System.Drawing.Point(1, 69)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(210, 101)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Gestione Operazioni"
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(177, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 20)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(6, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(165, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'cmbOperation
+        '
+        Me.cmbOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbOperation.FormattingEnabled = True
+        Me.cmbOperation.Items.AddRange(New Object() {"Spegni", "Riavvia", "Disconnetti", "Blocca", "Iberna", "Sospendi", "FAULT *** Esegui -> ***", "FAULT *** Interrompi processo -> ***"})
+        Me.cmbOperation.Location = New System.Drawing.Point(6, 19)
+        Me.cmbOperation.Name = "cmbOperation"
+        Me.cmbOperation.Size = New System.Drawing.Size(198, 21)
+        Me.cmbOperation.TabIndex = 4
+        '
+        'bttnEnableDisable
+        '
+        Me.bttnEnableDisable.Location = New System.Drawing.Point(6, 74)
+        Me.bttnEnableDisable.Name = "bttnEnableDisable"
+        Me.bttnEnableDisable.Size = New System.Drawing.Size(90, 21)
+        Me.bttnEnableDisable.TabIndex = 5
+        Me.bttnEnableDisable.Text = "Abilita"
+        Me.bttnEnableDisable.UseVisualStyleBackColor = True
+        '
+        'bttnClose
+        '
+        Me.bttnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.bttnClose.Location = New System.Drawing.Point(114, 74)
+        Me.bttnClose.Name = "bttnClose"
+        Me.bttnClose.Size = New System.Drawing.Size(90, 21)
+        Me.bttnClose.TabIndex = 6
+        Me.bttnClose.Text = "Chiudi"
+        Me.bttnClose.UseVisualStyleBackColor = True
+        '
+        'lblRemain
+        '
+        Me.lblRemain.BackColor = System.Drawing.Color.Transparent
+        Me.lblRemain.Font = New System.Drawing.Font("Tahoma", 9.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRemain.Location = New System.Drawing.Point(7, 184)
+        Me.lblRemain.Name = "lblRemain"
+        Me.lblRemain.Size = New System.Drawing.Size(204, 25)
+        Me.lblRemain.TabIndex = 7
+        Me.lblRemain.Text = "Secondi Rimasti: 0"
+        Me.lblRemain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblRemain.Visible = False
+        '
+        'timerOperate
+        '
+        Me.timerOperate.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(6, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Tempo in minuti"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtMin
+        '
+        Me.txtMin.BackColor = System.Drawing.Color.Chartreuse
+        Me.txtMin.Location = New System.Drawing.Point(95, 17)
+        Me.txtMin.Name = "txtMin"
+        Me.txtMin.Size = New System.Drawing.Size(105, 20)
+        Me.txtMin.TabIndex = 1
+        Me.txtMin.Text = "5"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.lblSec)
+        Me.GroupBox1.Controls.Add(Me.txtMin)
+        Me.GroupBox1.Location = New System.Drawing.Point(1, 1)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(210, 62)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Gestione Timer"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(211, 215)
+        Me.Controls.Add(Me.lblRemain)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Gestisci il Timer del tuo PC"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblSec As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents cmbOperation As System.Windows.Forms.ComboBox
+    Friend WithEvents bttnEnableDisable As System.Windows.Forms.Button
+    Friend WithEvents lblRemain As System.Windows.Forms.Label
+    Friend WithEvents bttnClose As System.Windows.Forms.Button
+    Friend WithEvents timerOperate As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtMin As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Timer PC/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim RemainTime As Double
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.cmbOperation.SelectedIndex = 0
+    End Sub
+    Private Sub bttnEnableDisable_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bttnEnableDisable.Click
+        'If Val(Me.txtMin.Text) <= 0 Then Me.txtMin.Focus() : Exit Sub
+        If cmbOperation.SelectedIndex = -1 Then MsgBox("Seleziona un'operazione", MsgBoxStyle.Exclamation, "Quindi?") : cmbOperation.Focus() : Exit Sub
+        If Me.bttnEnableDisable.Text = "Abilita" Then
+            Me.RemainTime = Val(Me.txtMin.Text) * 60
+            Me.lblRemain.Visible = True
+            Me.lblSec.Text = String.Format("in secondi: {0}", Val(Me.txtMin.Text) * 60)
+            Me.timerOperate.Start()
+            Me.bttnEnableDisable.Text = "Disabilita"
+            Me.bttnClose.Enabled = False
+            Me.txtMin.Enabled = False
+            Me.cmbOperation.Enabled = False
+        Else
+            Me.timerOperate.Stop()
+            Me.RemainTime = 0
+            Me.lblRemain.Visible = False
+            Me.lblRemain.Text = "Secondi Rimasti: 0"
+            Me.lblRemain.ForeColor = Color.Black
+            Me.txtMin.BackColor = Color.Chartreuse
+            Me.bttnEnableDisable.Text = "Abilita"
+            Me.bttnClose.Enabled = True
+            Me.txtMin.Enabled = True
+            Me.cmbOperation.Enabled = True
+        End If
+    End Sub
+    Private Sub txtMin_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtMin.KeyPress
+        If Not IsNumeric(e.KeyChar.ToString) And Not e.KeyChar = ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
+    End Sub
+    Private Sub txtMin_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtMin.TextChanged
+        Me.lblSec.Text = String.Format("in secondi: {0}", Val(Me.txtMin.Text) * 60)
+        ' Me.lblRemain.Text = String.Format("Second(s) Remain : {0}", Val(Me.txtMin.Text) * 60)
+        Me.RemainTime = Val(Me.txtMin.Text) * 60
+    End Sub
+    Private Sub timerOperate_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timerOperate.Tick
+        If Not RemainTime <= 0 Then
+            RemainTime -= 1
+            Me.lblRemain.Text = String.Format("Secondi Rimasti: {0}", RemainTime)
+            Exit Sub
+        ElseIf RemainTime = 0 Then
+            Me.Close()
+            If Me.cmbOperation.SelectedIndex = 0 Then 'Shutdown Computer
+                System.Diagnostics.Process.Start("shutdown.exe", "-s -t 0")
+            ElseIf Me.cmbOperation.SelectedIndex = 1 Then 'Restart Computer
+                System.Diagnostics.Process.Start("shutdown.exe", "-r -t 0")
+            ElseIf Me.cmbOperation.SelectedIndex = 2 Then 'Logoff Computer
+                System.Diagnostics.Process.Start("shutdown.exe", "/l /f")
+            ElseIf Me.cmbOperation.SelectedIndex = 3 Then ' Lock Workstation
+                'System.Diagnostics.Process.Start("")
+                Shell("Rundll32.exe User32.dll,LockWorkStation")
+            ElseIf Me.cmbOperation.SelectedIndex = 4 Then 'Hibernate Computer
+                Shell("rundll32.exe PowrProf.dll,SetSuspendState")
+            ElseIf Me.cmbOperation.SelectedIndex = 5 Then 'Sleep Computer
+                Shell("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+            ElseIf Me.cmbOperation.SelectedIndex = 6 Then 'Start Process
+            ElseIf Me.cmbOperation.SelectedIndex = 7 Then 'Kill Process
+                'task kill elenco processi
+            End If
+                Me.bttnEnableDisable.Text = "Abilita"
+                timerOperate.Stop()
+                Exit Sub
+            End If
+    End Sub
+    Private Sub bttnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bttnClose.Click
+        Me.Close()
+    End Sub
+    Private Sub lblRemain_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles lblRemain.TextChanged
+        If Me.lblRemain.Text = "Secondi Rimasti: 20" Then
+            Me.lblRemain.ForeColor = Color.Red
+            Me.txtMin.BackColor = Color.Red
+        End If
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        OpenFileDialog1.InitialDirectory = "Desktop"
+        OpenFileDialog1.Filter = "All Files (*.*)|"
+        OpenFileDialog1.FilterIndex = 1
+        TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+End Class
